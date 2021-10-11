afdd6e3b2f5a902b0ee3847b339710145b754068
rradhika1999 <39021888+rradhika1999@users.noreply.github.com>
2021-10-11 13:36:25 +0530

    Update nested branching.py

--- nested branching.py
+++ nested branching.py
@@ -1,6 +1,7 @@
-x,y,z = map(int, input()
+x,y,z = map(int, input().split())
 
 if x < y :
+  if y < z :
     print(x,y,z,'form a sorted sequence')
   else :
     print(x,y,z,'do not form a sorted sequence')
