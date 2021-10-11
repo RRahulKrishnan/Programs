2b710c84103183206532588703f73fdc15d42be2
RRahulKrishnan <87916269+RRahulKrishnan@users.noreply.github.com>
2021-10-11 13:33:58 +0530

    Create nested branching.py

---
+++ nested branching.py
@@ -0,0 +1,8 @@
+x,y,z = map(int, input()
+
+if x < y :
+    print(x,y,z,'form a sorted sequence')
+  else :
+    print(x,y,z,'do not form a sorted sequence')
+else :
+  print(x,'is not less than', y)
