790a59e204fe93e2b6d22798d957c3d2aa166145
RRahulKrishnan <87916269+RRahulKrishnan@users.noreply.github.com>
2021-10-11 13:43:24 +0530

    Create finding tuple size.py

---
+++ finding tuple size.py
@@ -0,0 +1,8 @@
+Tuple1 = ("A", 1, "B", 2, "C", 3)
+Tupel2 = ("Geek1", "Raju", "Geek2", "Nikhil, "Geek3", "Deepanshu")
+Tuple3 = ((1, "Lion"), ( 2, "Tiger"), (3, "Fox") (4, "Wolf"))
+  
+# printing the sizes of sample Tuples:
+print("Size of Tuple1: "(Tuple1.__sizeof__()) + "bytes")
+print("Size of Tuple2: "(Tuple2.__sizeof__()) + "bytes")
+print("Size of Tuple3: "(Tuple3.__sizeof__()) + "bytes")
