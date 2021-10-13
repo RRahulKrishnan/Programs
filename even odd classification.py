bd813896e1cf2bbde53aac0ee5f018374b025703
RRahulKrishnan <87916269+RRahulKrishnan@users.noreply.github.com>
2021-10-13 14:01:00 +0530

    Create even odd classification.py

---
+++ even odd classification.py
@@ -0,0 +1,5 @@
+w = int(input("ENTER THE VALUE OF THE WEIGHT OF THE OBJECT:  "))
+if (w % 2)==0: #differentiates if the number is even or bad.
+    print("YES")
+else: #if the number is odd then the value will be printed as no.
+    print("NO")  
