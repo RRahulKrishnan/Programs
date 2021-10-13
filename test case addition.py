a1a5c9ed0da733670d6873f97175704e9d3f93fd
RRahulKrishnan <87916269+RRahulKrishnan@users.noreply.github.com>
2021-10-13 14:01:40 +0530

    Create test case addition.py

---
+++ test case addition.py
@@ -0,0 +1,4 @@
+t = int(input("ENTER THE NUMBER OF TEST CASES: "))
+for i in range(t) :  #basically takes the no of inputs in the  program.
+  a,b = map(int, input().split()) #takes the input from the user.
+  print(a+b) #prints the number that was given by the user as the input. 
